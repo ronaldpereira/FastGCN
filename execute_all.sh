db7d1fb027b1b3a457df9d7350fff7879d4d59df
Ronald Pereira <ronald.pereira@ifood.com.br>
2020-09-01 09:05:11 -0300

    added amlsim_10k to execute all script

--- execute_all.sh
+++ execute_all.sh
@@ -1,13 +1,21 @@
-python pubmed_Mix.py --dataset amlsim_sample > output/amlsim_sample_Mix.txt
-python pubmed_Mix_sampleA.py --dataset amlsim_sample > output/amlsim_sample_Mix_sampleA.txt
-python pubmed_Mix_uniform.py --dataset amlsim_sample > output/amlsim_sample_uniform.txt
-python pubmed-original_transductive_FastGCN.py --dataset amlsim_sample > output/amlsim_sample_original_transductive_fastgcn.txt
-python pubmed-original_inductive_FastGCN.py --dataset amlsim_sample > output/amlsim_sample_inductive_fastgcn.txt
-python pubmed_inductive_appr2layers.py --dataset amlsim_sample > output/amlsim_sample_inductive_appr2layers.txt
+# python pubmed_Mix.py --dataset amlsim_sample > output/amlsim_sample_Mix.txt
+# python pubmed_Mix_sampleA.py --dataset amlsim_sample > output/amlsim_sample_Mix_sampleA.txt
+# python pubmed_Mix_uniform.py --dataset amlsim_sample > output/amlsim_sample_uniform.txt
+# python pubmed-original_transductive_FastGCN.py --dataset amlsim_sample > output/amlsim_sample_original_transductive_fastgcn.txt
+# python pubmed-original_inductive_FastGCN.py --dataset amlsim_sample > output/amlsim_sample_inductive_fastgcn.txt
+# python pubmed_inductive_appr2layers.py --dataset amlsim_sample > output/amlsim_sample_inductive_appr2layers.txt
 
-python pubmed_Mix.py --dataset amlsim > output/amlsim_Mix.txt
-python pubmed_Mix_sampleA.py --dataset amlsim > output/amlsim_Mix_sampleA.txt
-python pubmed_Mix_uniform.py --dataset amlsim > output/amlsim_uniform.txt
-python pubmed-original_transductive_FastGCN.py --dataset amlsim > output/amlsim_original_transductive_fastgcn.txt
-python pubmed-original_inductive_FastGCN.py --dataset amlsim > output/amlsim_inductive_fastgcn.txt
-python pubmed_inductive_appr2layers.py --dataset amlsim > output/amlsim_inductive_appr2layers.txt
+# python pubmed_Mix.py --dataset amlsim > output/amlsim_Mix.txt
+# python pubmed_Mix_sampleA.py --dataset amlsim > output/amlsim_Mix_sampleA.txt
+# python pubmed_Mix_uniform.py --dataset amlsim > output/amlsim_uniform.txt
+# python pubmed-original_transductive_FastGCN.py --dataset amlsim > output/amlsim_original_transductive_fastgcn.txt
+# python pubmed-original_inductive_FastGCN.py --dataset amlsim > output/amlsim_inductive_fastgcn.txt
+# python pubmed_inductive_appr2layers.py --dataset amlsim > output/amlsim_inductive_appr2layers.txt
+
+python pubmed_Mix.py --dataset amlsim_10k > output/amlsim_10k_Mix.txt
+python pubmed_Mix_sampleA.py --dataset amlsim_10k > output/amlsim_10k_Mix_sampleA.txt
+python pubmed_Mix_uniform.py --dataset amlsim_10k > output/amlsim_10k_uniform.txt
+python pubmed-original_transductive_FastGCN.py --dataset amlsim_10k > output/amlsim_10k_original_transductive_fastgcn.txt
+python pubmed-original_inductive_FastGCN.py --dataset amlsim_10k > output/amlsim_10k_inductive_fastgcn.txt
+python pubmed_inductive_appr2layers.py --dataset amlsim_10k > output/amlsim_10k_inductive_appr2layers.txt
+
